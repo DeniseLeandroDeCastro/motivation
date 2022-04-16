e7cd700cefc705417895e9765fbf6159d482523b
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-04-16 19:53:20 -0300

    Layout da MainActivity concluído

--- app/src/main/java/com/example/motivation/MainActivity.kt
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Esconder a actionBar
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Eventos
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_phrase) {
+            var s = ""
+        }
     }
 }
